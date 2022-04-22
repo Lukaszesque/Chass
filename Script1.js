91956a062fb6310538f0efab3dae164c0737bf28
Lukasz.Gocek <lukasz.gocek@geeks.ltd.uk>
2022-04-22 16:02:03 +0100

    pawn svg implemented

--- Script1.js
+++ Script1.js
@@ -1,10 +1,15 @@
 
 
   var items = document.querySelectorAll(".container div");
-  console.log(items);
+  //console.log(items);
   for (var i = 0; i < items.length; i++)
   {
     var item = items[i];
-    console.log(item, item.id)
+    //console.log(item, item.id)
     item.style.gridArea = item.id;
   }
+  const pawn = document.createElement("img");
+    pawn.src = "Icons/pawn.svg";
+    //console.log("hi");
+    document.getElementById('a2').appendChild(pawn);
+ 
